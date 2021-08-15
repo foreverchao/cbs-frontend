0cb05f1d9e9fb825a1cb4bbdaa4193dd7ae874da
foreverchao <49773012+foreverchao@users.noreply.github.com>
2021-08-15 19:00:30 +0800

    1

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -108,7 +108,7 @@ export default {
           password:this.password,
         })
         .then((res) => {
-           //console.log(res)
+           console.log(res)
            this.$store.commit("changeToken", res.data.token);
            this.$router.push("/home");
         })
